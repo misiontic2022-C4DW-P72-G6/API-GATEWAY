7eeebf014591638160da4cb55bdfb662cf05f5f6
Neixrinf <83479529+neixrinf@users.noreply.github.com>
2021-11-29 15:51:32 -0500

    Update server.js
    se modifica linea auth api, pendiente el account

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 module.exports = {
-    auth_api_url: 'https://auth-ms-mt.herokuapp.com',
+    auth_api_url: ' https://userc4dw-db.herokuapp.com',
     account_api_url: 'https://account-ms-mt.herokuapp.com',
 };
